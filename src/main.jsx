dd182da355ee2ddd5472c8fb11e410ae2c5c6ec6
choihongjun78 <redhong2012@naver.com>
2025-05-30 14:09:29 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import LUSYDashboard from './LUSYDashboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LUSYDashboard />
+  </React.StrictMode>
+)
